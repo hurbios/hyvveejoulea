529a843f014595d8a4e9a60005c2a4494578a0d7
hurbios <15719468+hurbios@users.noreply.github.com>
2024-12-05 23:46:09 +0200

    add day4.2

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -8,21 +8,27 @@ def readFile(file_path):
 
 
 def check_xmas(x, y, dirx, diry, rows):
-    for i,char in enumerate("XMAS"):
-        #print(len(rows), y + diry * i)
+    # for i,char in enumerate("XMAS"):
+    for i,char in enumerate("MAS"):
         if y + diry * i < 0 or len(rows) <= y + diry * i or len(rows[y + diry * i]) <= x + dirx * i or x + dirx * i < 0 or char != rows[y + diry * i][x + dirx * i]:
             return False
     return True
 
 def get_xmas_number(x, y, rows):
-    amount = 1 if check_xmas(x,y,1,1,rows) else 0       # right-down
-    amount += 1 if check_xmas(x,y,1,-1,rows) else 0     # right-up
-    amount += 1 if check_xmas(x,y,-1,1,rows) else 0     # left-down
-    amount += 1 if check_xmas(x,y,-1,-1,rows) else 0    # left-up
-    amount += 1 if check_xmas(x,y,-1,0,rows) else 0     # left
-    amount += 1 if check_xmas(x,y,1,0,rows) else 0      # right
-    amount += 1 if check_xmas(x,y,0,1,rows) else 0      # down
-    amount += 1 if check_xmas(x,y,0,-1,rows) else 0     # up
+    amount = 1 if check_xmas(x,y,1,1,rows) and check_xmas(x,y+2,1,-1,rows) else 0
+    amount += 1 if check_xmas(x,y,-1,1,rows) and check_xmas(x,y+2,-1,-1,rows) else 0
+    amount += 1 if check_xmas(x,y,1,1,rows) and check_xmas(x+2,y,-1,1,rows) else 0
+    amount += 1 if check_xmas(x,y,1,-1,rows) and check_xmas(x+2,y,-1,-1,rows) else 0
+    
+    # obsolete 1st part:
+    # amount = 1 if check_xmas(x,y,1,1,rows) else 0       # right-down
+    # amount += 1 if check_xmas(x,y,1,-1,rows) else 0     # right-up
+    # amount += 1 if check_xmas(x,y,-1,1,rows) else 0     # left-down
+    # amount += 1 if check_xmas(x,y,-1,-1,rows) else 0    # left-up
+    # amount += 1 if check_xmas(x,y,-1,0,rows) else 0     # left
+    # amount += 1 if check_xmas(x,y,1,0,rows) else 0      # right
+    # amount += 1 if check_xmas(x,y,0,1,rows) else 0      # down
+    # amount += 1 if check_xmas(x,y,0,-1,rows) else 0     # up
     return amount
 
 def main():
@@ -33,8 +39,9 @@ def main():
     for y, row in enumerate(rows):
         #print(y)
         for x, char in enumerate(row):
-            if char == "X":
-                xmas_amounts.append(get_xmas_number(x, y, rows))
+            # if char == "X":
+            #     xmas_amounts.append(get_xmas_number(x, y, rows))
+            xmas_amounts.append(get_xmas_number(x, y, rows))
     
     print(f"xmas amount: {sum(xmas_amounts)}")
         
