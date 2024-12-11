75798da1f0a45a2f01ce2682834f3cb25dd4c1ac
hurbios <15719468+hurbios@users.noreply.github.com>
2024-12-11 15:24:49 +0200

    add day11.2

--- src/day11/day11.py
+++ src/day11/day11.py
@@ -1,3 +1,5 @@
+import math
+
 def readFile(file_path):
     sequence = []
     with open(file_path, 'r') as file:
@@ -7,29 +9,52 @@ def readFile(file_path):
 
     return sequence
 
+def get_divided(digits, stone):
+    half_length = digits//2
+    divider = math.pow(10, half_length)
+    stone1 = int(stone // divider)
+    stone2 = int(stone % divider)
+    return [stone1, stone2]
+
 def apply_stone_rules(stone):
     if stone == 0:
         return [1]
-    str_stone = str(stone)
-    if len(str_stone) % 2 == 0:
-        half_length = len(str_stone)//2
-        return [int(str_stone[:half_length]), int(str_stone[half_length:])]
+
+    digits = int(math.log10(stone)) + 1
+    if digits % 2 == 0:
+        return get_divided(digits, stone)
+
     return [stone*2024]
+
 
 
 def main():
     #stones = readFile("./test_input.txt")
     stones = readFile("./input.txt")
+    stone_amounts = {}
+    for stone in stones:
+        if stone not in stone_amounts:
+            stone_amounts[stone] = 1
 
-    for i in range(25):
-        print(i)
-        second_stones = []
-        for stone in stones:
-            for new_stone in apply_stone_rules(stone):
-                second_stones.append(new_stone)
-        stones = second_stones
-        #print(stones)
-    print(len(stones))
+    stone_result_cache = {}
+
+    print(stone_amounts)
+    for i in range(75):
+        print(f"--- {i} ---")
+        second_stones = {}
+        for stone in stone_amounts.items():
+            #print(stone)
+            if stone[0] not in stone_result_cache:
+                stone_result_cache[stone[0]] = apply_stone_rules(stone[0])
+                #print(stone_result_cache[stone[0]])
+            for new_stone in stone_result_cache[stone[0]]:
+                if new_stone not in second_stones:
+                    second_stones[new_stone] = stone[1]
+                else:
+                    second_stones[new_stone] += stone[1]
+        stone_amounts = second_stones
+    print("####")
+    print(f"amount of stones: {sum(stone_amounts.values())}")
 
 
 if __name__ == "__main__":
