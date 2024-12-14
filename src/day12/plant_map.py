7c3f3945f98c69e607fe207c7188136efec9c8ca
hurbios <15719468+hurbios@users.noreply.github.com>
2024-12-14 20:04:30 +0200

    add day12.2

--- src/day12/plant_map.py
+++ src/day12/plant_map.py
@@ -4,6 +4,8 @@ class PlantMap:
         self._plants = {}
         self._plant_amount = {}
         self._plant_fences = {}
+        self._max_y = len(self._plantmap) - 1
+        self._max_x = len(self._plantmap[0]) - 1 if self._max_y > 0 else -1
 
         for y,p in enumerate(self._plantmap):
             for x in range(len(p)):
@@ -87,12 +89,51 @@ class PlantMap:
         # if plant_type == "I":
         #     print("AREAS",neighbour_areas, self._plant_fences, self._plant_amount, self._plants)
 
+    def _is_same_plant(self, position, plant_type):
+        if position[1] < 0 or position[0] < 0 or position[1] > self._max_y or position[0] > self._max_x:
+            return False
+        return plant_type == self._plantmap[position[1]][position[0]]
+
+    def _get_areas_straight_fences(self, area, plant_type):
+        corners = 0
+        for plant in area:
+            if not self._is_same_plant((plant[0], plant[1] - 1), plant_type) and not self._is_same_plant((plant[0] - 1, plant[1]), plant_type):
+                corners += 1
+            if not self._is_same_plant((plant[0], plant[1] - 1), plant_type) and not self._is_same_plant((plant[0] + 1, plant[1]), plant_type):
+                corners += 1
+            if not self._is_same_plant((plant[0], plant[1] + 1), plant_type) and not self._is_same_plant((plant[0] - 1, plant[1]), plant_type):
+                corners += 1
+            if not self._is_same_plant((plant[0], plant[1] + 1), plant_type) and not self._is_same_plant((plant[0] + 1, plant[1]), plant_type):
+                corners += 1
+
+            if self._is_same_plant((plant[0], plant[1] - 1), plant_type) and self._is_same_plant((plant[0] - 1, plant[1]), plant_type) and not self._is_same_plant((plant[0] - 1, plant[1] - 1), plant_type):
+                #print("a",(plant[0], plant[1]))
+                corners += 1
+            if self._is_same_plant((plant[0], plant[1] - 1), plant_type) and self._is_same_plant((plant[0] + 1, plant[1]), plant_type) and not self._is_same_plant((plant[0] + 1, plant[1] - 1), plant_type):
+                #print("b",(plant[0], plant[1]))
+                corners += 1
+            if self._is_same_plant((plant[0], plant[1] + 1), plant_type) and self._is_same_plant((plant[0] - 1, plant[1]), plant_type) and not self._is_same_plant((plant[0] - 1, plant[1] + 1), plant_type):
+                #print("c",(plant[0], plant[1]))
+                corners += 1
+            if self._is_same_plant((plant[0], plant[1] + 1), plant_type) and self._is_same_plant((plant[0] + 1, plant[1]), plant_type) and not self._is_same_plant((plant[0] + 1, plant[1] + 1), plant_type):
+                #print("d",(plant[0], plant[1]))
+                corners += 1
+        #print("planttype",plant_type,"corners", corners)
+        return corners
 
 
     def get_plant_scores(self):
         score = 0
+        score2 = 0
         for plant_type in self._plants.keys():
             for i in range(len(self._plants[plant_type])):
                 print(f"{plant_type}[{i}], {self._plant_amount[plant_type][i]} * {self._plant_fences[plant_type][i]}  = {self._plant_amount[plant_type][i] * self._plant_fences[plant_type][i]}")
                 score += self._plant_amount[plant_type][i] * self._plant_fences[plant_type][i]
+        print("---")
+        for plant_type in self._plants.keys():
+            for i in range(len(self._plants[plant_type])):
+                score2 += self._get_areas_straight_fences(self._plants[plant_type][i], plant_type) * self._plant_amount[plant_type][i]
+                print(self._get_areas_straight_fences(self._plants[plant_type][i], plant_type) * self._plant_amount[plant_type][i])
+        print("score2", score2)
+        print("---")
         return score
