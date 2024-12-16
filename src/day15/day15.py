66ec23ecc9dec8bc85deddabdfd00991c895dd0d
hurbios <15719468+hurbios@users.noreply.github.com>
2024-12-16 21:36:46 +0200

    add day15.2

--- src/day15/day15.py
+++ src/day15/day15.py
@@ -1,4 +1,9 @@
+import sys, time
+# import pygame
 from gamemap import GameMap
+
+print(sys.getrecursionlimit())
+# sys.setrecursionlimit(10000)
 
 def readFile(file_path):
     gamemap = []
@@ -14,19 +19,53 @@ def readFile(file_path):
                 moves+=row.replace("\n", "")
     return gamemap, moves
 
+def input_direction(key):
+    print(key if key == "^[[" else 0)
+
+
 def run():
+    # gamemadata, moves = readFile("./test_input4.txt")
+    # gamemadata, moves = readFile("./test_input3.txt")
     # gamemadata, moves = readFile("./test_input2.txt")
     # gamemadata, moves = readFile("./test_input.txt")
     gamemadata, moves = readFile("./input.txt")
+    # pygame.init()
     gamemap = GameMap(gamemadata)
-    # gamemap.print_map()
-
+    gamemap.print_map()
+    moveslen = len(moves)
+    i=0
     for move in moves:
-        # print(f"move {move}")
+        i+=1
+        print(f"{i}/{moveslen}, move {move}")
         gamemap.move_robot(move)
-        # gamemap.print_map()
+        # print(gamemap.print_map())
+        # sys.stdout.write(gamemap.print_map())
+        # sys.stdout.flush()
+        # time.sleep(0.5)
+    # while True:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.KEYDOWN:
+    #             ch = False
+    #             if event.key == pygame.K_LEFT:
+    #                 ch = "<"
+    #             elif event.key == pygame.K_RIGHT:
+    #                 ch = ">"
+    #             elif event.key == pygame.K_UP:
+    #                 ch = "^"
+    #             elif event.key == pygame.K_DOWN:
+    #                 ch = "v"
+    #             if ch:
+    #                 gamemap.move_robot(ch)
+
+    #     # gamemap.print_map()
+    #     sys.stdout.write(gamemap.print_map())
+    #     sys.stdout.flush()
+    #     time.sleep(0.5)
+
 
     print(gamemap.get_GPS_sum())
 
 if __name__ == "__main__":
     run()
+
+# just awful code but gets it done...
--- src/day15/gamemap.py
+++ src/day15/gamemap.py
@@ -1,3 +1,77 @@
+class Box:
+    def __init__(self, position):
+        self._position = position
+
+    def in_position(self, position):
+        if self._position == position or self._position == (position[0] - 1, position[1]):
+            return True
+        return False
+
+    def graph(self, position):
+        if self._position == position:
+            return "["
+        elif self._position == (position[0] - 1, position[1]):
+            return "]"
+
+    def get_box_score(self):
+        return self._position[1]*100 + self._position[0]
+
+    def _box_in_position(self, position, boxes):
+        for box in boxes:
+            if box.in_position(position):
+                return box
+        return False
+
+    def can_move(self, position, direction, boxes, walls):
+        new_pos = new_pos2 = False
+        if direction[2] == "<":
+            new_pos = (self._position[0] + direction[0], self._position[1] + direction[1])
+        elif direction[2] == ">":
+            new_pos = (self._position[0] + direction[0] + 1, self._position[1] + direction[1])
+        elif direction[2] in ["^","v"]:
+            new_pos = (self._position[0] + direction[0], self._position[1] + direction[1])
+            new_pos2 = (self._position[0] + direction[0] + 1, self._position[1] + direction[1])
+
+        if new_pos in walls or new_pos2 in walls:
+            # print("false")
+            return False
+        box_in_pos = self._box_in_position(new_pos, boxes)
+        box_in_pos2 = False
+        if new_pos2:
+            box_in_pos2 = self._box_in_position(new_pos2, boxes)
+        if box_in_pos and box_in_pos2 and box_in_pos != box_in_pos2:
+            return box_in_pos.can_move(new_pos, direction, boxes, walls) and box_in_pos2.can_move(new_pos2, direction, boxes, walls)
+        elif box_in_pos:
+            return box_in_pos.can_move(new_pos, direction, boxes, walls)
+        elif box_in_pos2:
+            return box_in_pos2.can_move(new_pos2, direction, boxes, walls)
+        return True
+        
+    def move(self, direction, boxes):
+        new_pos = new_pos2 = False
+        if direction[2] == "<":
+            new_pos = (self._position[0] + direction[0], self._position[1] + direction[1])
+        elif direction[2] == ">":
+            new_pos = (self._position[0] + direction[0] + 1, self._position[1] + direction[1])
+        elif direction[2] in ["^","v"]:
+            new_pos = (self._position[0] + direction[0], self._position[1] + direction[1])
+            new_pos2 = (self._position[0] + direction[0] + 1, self._position[1] + direction[1])
+        box_in_pos = self._box_in_position(new_pos, boxes)
+        box_in_pos2 = False
+        if new_pos2:
+            box_in_pos2 = self._box_in_position(new_pos2, boxes)
+        
+        self._position = (self._position[0] + direction[0], self._position[1] + direction[1])
+
+        if box_in_pos and box_in_pos2 and box_in_pos != box_in_pos2:
+            box_in_pos.move(direction, boxes)
+            box_in_pos2.move(direction, boxes)
+        elif box_in_pos:
+            box_in_pos.move(direction, boxes)
+        elif box_in_pos2:
+            box_in_pos2.move(direction, boxes)
+        
+
 class GameMap:
     def __init__(self, mapdata):
         self._mapdata = mapdata
@@ -9,6 +83,23 @@ class GameMap:
         self._setup()
 
     def _setup(self):
+        newmap = []
+        for row in self._mapdata:
+            newrow = ""
+            for col in row:
+                if col == ".":
+                    newrow += ".."
+                elif col == "#":
+                    newrow += "##"
+                elif col == "O":
+                    newrow += "O."
+                elif col == "@":
+                    newrow += "@."
+            newmap.append(newrow)
+        self._mapdata = newmap
+        self._max_y = len(self._mapdata) - 1
+        self._max_x = len(self._mapdata[0]) - 1 if self._max_y >= 0 else -1
+
         for y,row in enumerate(self._mapdata):
             for x,col in enumerate(row):
                 if col == "#":
@@ -16,36 +107,63 @@ class GameMap:
                 elif col == "@":
                     self._robot = (x,y)
                 elif col == "O":
-                    self._boxes.append((x,y))
+                    self._boxes.append(Box((x,y)))
     
-    def _next_empty_position(self, direction):
+    # def _next_empty_position(self, direction):
+    #     position = self._robot
+    #     if direction == "^":
+    #         for pos_y in range(position[1] - 1, -1, -1):
+    #             if (position[0], pos_y) in self._walls:
+    #                 return False
+    #             elif not self._box_in_position((position[0], pos_y)):
+    #                 return (position[0], pos_y)
+    #     elif direction == "v":
+    #         for pos_y in range(position[1] + 1, self._max_y + 1, 1):
+    #             if (position[0], pos_y) in self._walls:
+    #                 return False
+    #             elif not self._box_in_position((position[0], pos_y)):
+    #                 return (position[0], pos_y)
+    #     elif direction == "<":
+    #         for pos_x in range(position[0] - 1, - 1, -1):
+    #             if (pos_x, position[1]) in self._walls:
+    #                 return False
+    #             elif not self._box_in_position((pos_x, position[1])):
+    #                 return (pos_x, position[1])
+    #     elif direction == ">":
+    #         for pos_x in range(position[0] + 1, self._max_x + 1, 1):
+    #             if (pos_x, position[1]) in self._walls:
+    #                 return False
+    #             elif not self._box_in_position((pos_x, position[1])):
+    #                 return (pos_x, position[1])
+    
+    def _can_move(self, direction):
         position = self._robot
         if direction == "^":
-            for pos_y in range(position[1] - 1, -1, -1):
-                if (position[0], pos_y) in self._walls:
-                    return False
-                elif (position[0], pos_y) not in self._boxes:
-                    return (position[0], pos_y)
+            box_in_pos = self._box_in_position((position[0], position[1] - 1))
+            if (position[0], position[1] - 1) in self._walls:
+                return False
+            elif box_in_pos:
+                return box_in_pos.can_move((position[0], position[1] - 1), (0, -1, "^"), self._boxes, self._walls)
         elif direction == "v":
-            for pos_y in range(position[1] + 1, self._max_y + 1, 1):
-                if (position[0], pos_y) in self._walls:
-                    return False
-                elif (position[0], pos_y) not in self._boxes:
-                    return (position[0], pos_y)
+            box_in_pos = self._box_in_position((position[0], position[1] + 1))
+            if (position[0], position[1] + 1) in self._walls:
+                return False
+            elif box_in_pos:
+                return box_in_pos.can_move((position[0], position[1] + 1), (0, 1, "v"), self._boxes, self._walls)
         elif direction == "<":
-            for pos_x in range(position[0] - 1, - 1, -1):
-                if (pos_x, position[1]) in self._walls:
-                    return False
-                elif (pos_x, position[1]) not in self._boxes:
-                    return (pos_x, position[1])
+            box_in_pos = self._box_in_position((position[0] - 1, position[1]))
+            if (position[0] - 1, position[1]) in self._walls:
+                return False
+            elif box_in_pos:
+                return box_in_pos.can_move((position[0] - 1, position[1]), (-1, 0, "<"), self._boxes, self._walls)
         elif direction == ">":
-            for pos_x in range(position[0] + 1, self._max_x + 1, 1):
-                if (pos_x, position[1]) in self._walls:
-                    return False
-                elif (pos_x, position[1]) not in self._boxes:
-                    return (pos_x, position[1])
+            box_in_pos = self._box_in_position((position[0] + 1, position[1]))
+            if (position[0] + 1, position[1]) in self._walls:
+                return False
+            elif box_in_pos:
+                return box_in_pos.can_move((position[0] + 1, position[1]), (1, 0, ">"), self._boxes, self._walls)
        
-        return False
+        return True
 
     def _get_next_space(self, direction):
         if direction == "^":
@@ -58,38 +176,55 @@ class GameMap:
             return (self._robot[0] + 1, self._robot[1])
         else:
             return False
+        
+    def _box_in_position(self, position):
+        for box in self._boxes:
+            if box.in_position(position):
+                return box
+        return False
 
     def move_robot(self, direction):
         # print("robot:",self._robot)
-        next_empty = self._next_empty_position(direction)
-        if next_empty:
+        can_move = self._can_move(direction)
+        # print("can_move", can_move)
+        if can_move:
             next_space = self._get_next_space(direction)
             # print("goto",next_space,"move block", next_empty, "robot:",self._robot)
-            if next_space in self._boxes:
-                self._boxes.remove(next_space)
-                self._boxes.append(next_empty)
+            box_in_pos = self._box_in_position(next_space)
+            if box_in_pos:
+                if direction == "^":
+                    box_dir = (0,-1,"^")
+                elif direction == "v":
+                    box_dir = (0,1,"v")
+                elif direction == "<":
+                    box_dir = (-1,0,"<")
+                elif direction == ">":
+                    box_dir = (1,0,">")
+                else:
+                    box_dir = "issue"
+                box_in_pos.move(box_dir, self._boxes)
             self._robot = next_space
 
     def get_GPS_sum(self):
         # print(self._boxes)
         result = 0
         for box in self._boxes:
-            result += box[1]*100 + box[0]
+            result += box.get_box_score()
         return result
     
     def print_map(self):
         graph = ""
-        for row in range(0,self._max_y + 1):
-            graph+="#"
-            for col in range(1,self._max_y):
+        for row in range(self._max_y + 1):
+            for col in range(self._max_x + 1):
+                box_in_pos = self._box_in_position((col,row))
                 if (col,row) == self._robot:
                     graph+="@"
-                elif (col,row) in self._boxes:
-                    graph+="O"
+                elif box_in_pos:
+                    graph+=box_in_pos.graph((col,row))
                 elif (col,row) in self._walls:
                     graph+="#"
                 else:
                     graph+="."
-            graph+="#"
             graph+="\n"
-        print(graph)
+        # print(graph)
+        return graph
